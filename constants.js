5dcc9d942ca223e6f530cee2bd9475b9c95b6923
Velayutham87 <vetri2vetri@gmail.com>
2025-05-27 08:31:32 +0530

    tafmIndex-basicFormAndConstants

---
+++ constants.js
@@ -0,0 +1,7 @@
+export const maxCapabilityScore = {
+  TDC: 41, //Test design capability
+  TDMC: 13, //Test data management capability
+  TEC: 17, //Test execution capability
+  TRC: 16, // Test reporting capability
+  TFIC: 8, //Test Framework Integration capability
+};
