e5432c1b1ef0a1bdca51343b3d98fcabfd919dd0
Velayutham87 <vetri2vetri@gmail.com>
2025-07-09 11:03:06 +0530

    Fixed-Scores logic

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
   const tafmModal = new bootstrap.Modal(document.getElementById('tafmIndexModal'));
   const showMoreDetails = document.getElementById('btnShowMoreDetails');
   const maxCapabilityScore = {
-    TDC: 41, //Test design capability
-    TDMC: 13, //Test data management capability
+    TDC: 38, //Test design capability
+    TDMC: 14, //Test data management capability
     TEC: 17, //Test execution capability
     TRC: 16, // Test reporting capability
-    TFIC: 8, //Test Framework Integration capability
+    TFIC: 15, //Test Framework Integration capability
   };
 
   let capabilityWiseScores = null; // Store latest scores here
